9c1677523f9d2d8b6f2d061cfb87bf917ef41423
Ingo Wechsung <ingo.wechsung@gmail.com>
2024-01-05 12:58:11 +0100

    after lake init

---
+++ lakefile.lean
@@ -0,0 +1,9 @@
+import Lake
+open Lake DSL
+
+package «Tutorial» where
+  -- add package configuration options here
+
+@[default_target]
+lean_lib «Tutorial» where
+  -- add library configuration options here
